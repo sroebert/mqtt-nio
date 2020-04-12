605a802df6ce58635a159b068e6966729a76617e
Steven Roebert <steven@roebert.nl>
2020-04-12 17:25:13 +0200

    Updated travis file

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "mqtt-nio",
     platforms: [
-       .macOS(.v10_15)
+       .macOS(.v10_14)
     ],
     products: [
         .library(name: "MQTTNIO", targets: ["MQTTNIO"]),
