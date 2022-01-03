20fc4ede4950ca6b0c41f2c3c3e246c6f56bbabe
Steven Roebert <steven@roebert.nl>
2022-01-03 16:34:31 +0100

    Updated concurrency to work with new Xcode 13.2.1 for older mac/ios versions

--- Sources/MQTTNIO/MQTTClient+Concurrency.swift
+++ Sources/MQTTNIO/MQTTClient+Concurrency.swift
@@ -1,9 +1,9 @@
 import Foundation
 import NIO
 
-#if compiler(>=5.5) && canImport(_Concurrency)
+#if compiler(>=5.5.2) && canImport(_Concurrency)
 
-@available(macOS 12.0, iOS 15.0, watchOS 8.0, tvOS 15.0, *)
+@available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
 extension MQTTClient {
     
     // MARK: - Connection
